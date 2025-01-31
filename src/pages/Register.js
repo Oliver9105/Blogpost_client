60c65d874bd81034233379a41e3f06ed7b6e3ea2
Oliver cheruiyot <olivercher1000@gmail.com>
2025-01-31 21:14:06 +0300

    implement Formik and Yup for form management and validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,18 +28,11 @@ const Register = () => {
       try {
         const response = await fetch("http://localhost:5555/api/register", {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
         });
-
         const data = await response.json();
-
-        if (!response.ok) {
-          throw new Error(data.error || "Registration failed");
-        }
-
+        if (!response.ok) throw new Error(data.error || "Registration failed");
         alert(data.message); // Show success message
         navigate("/login"); // Redirect to login page
       } catch (error) {
