34b54a343096b96aa12585e5df1d81d9cc468d96
Oliver Cheruiyot <olivercher1000@gmail.com>
2025-08-21 21:18:18 +0300

    update routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import CreatePost from "./pages/CreatePost";
 import ViewPost from "./pages/ViewPost";
-import "./index.css";
+import EditPost from "./pages/EditPost";
+import SignIn from "./pages/SignIn";
+// import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/create" element={<CreatePost />} />
         <Route path="/posts/:id" element={<ViewPost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
   );
